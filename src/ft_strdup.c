e0afeeb9a214f92f18a1ec8e19c9b41916da1a9c
Nikolaii <guillaumelodenos@gmail.com>
2016-01-31 22:26:38 +0100

    libft

---
+++ src/ft_strdup.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glodenos <glodenos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2100/01/01 00:00:00 by glodenos          #+#    #+#             */
+/*   Updated: 2100/01/01 00:00:00 by glodenos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(char const *s)
+{
+	char	*tmp;
+
+	if (!s)
+		return (NULL);
+	tmp = (char *)malloc(sizeof(*tmp) * ft_strlen(s) + 1);
+	return (tmp = ft_strcpy(tmp, s));
+}
