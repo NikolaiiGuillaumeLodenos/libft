611b9f88590adfc1e7370f86f2bad4478b99f1dd
Guillaume LODENOS <glodenos@e2r4p19.42.fr>
2016-09-05 05:37:40 +0200

    libft

--- src/ft_strsub.c
+++ src/ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: glodenos <glodenos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2100/01/01 00:00:00 by glodenos          #+#    #+#             */
-/*   Updated: 2016/09/02 09:14:06 by glodenos         ###   ########.fr       */
+/*   Updated: 2016/09/05 05:14:51 by glodenos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,9 @@ char		*ft_strsub(char const *s, unsigned int start, size_t len)
 	char	*tmp;
 
 	i = -1;
-	if (s == NULL || start > ft_strlen(s) ||
-		!(tmp = (char*)ft_memalloc(len + 1)))
+	if ((!*s && !s) || start > ft_strlen(s) || !(tmp = (char*)ft_memalloc(len + 1)))
 		return (NULL);
 	while (++i < len && s[start] != '\0')
 		tmp[i] = s[start++];
-	tmp[i] = '\0';
 	return (tmp);
 }
--- src/get_next_line.c
+++ src/get_next_line.c
@@ -6,26 +6,23 @@
 /*   By: glodenos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/28 11:22:00 by glodenos          #+#    #+#             */
-/*   Updated: 2016/09/05 02:08:38 by glodenos         ###   ########.fr       */
+/*   Updated: 2016/09/05 05:07:11 by glodenos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int		get_read(int const fd, char **over)
+static int	get_read(int const fd, char **over)
 {
-	int			i;
-	char		*tmp;
-	char		*swp;
+	int		i;
+	char	*tmp;
+	char	*swp;
 
 	i = 0;
-	if (!(tmp = ft_memalloc(BUFF_SIZE + 1)))
+	if (!(tmp = (char*)ft_memalloc(sizeof(char) * (BUFF_SIZE + 1))))
 		return (-1);
 	if ((i = read(fd, tmp, BUFF_SIZE)) == -1)
-	{
-		free(tmp);
 		return (-1);
-	}
 	if (!over[fd])
 		over[fd] = ft_strdup(tmp);
 	else
@@ -38,10 +35,10 @@ static int		get_read(int const fd, char **over)
 	return (i);
 }
 
-static int		get_line_over(int const fd, char **over, char **line, int k)
+static int	get_line_over(int const fd, char **over, char **line, int k)
 {
-	int			i;
-	char		*tmp;
+	int		i;
+	char	*tmp;
 
 	i = 0;
 	while (over[fd][i] != '\n' && over[fd][i])
@@ -56,7 +53,7 @@ static int		get_line_over(int const fd, char **over, char **line, int k)
 	return (i);
 }
 
-int				get_next_line(int const fd, char **line)
+int			get_next_line(int const fd, char **line)
 {
 	int			i;
 	int			k;
@@ -69,9 +66,6 @@ int				get_next_line(int const fd, char **line)
 	while ((i = get_line_over(fd, over, line, k)) == -1)
 		k = get_read(fd, over);
 	if (!over[fd] && !k && !i)
-	{
-		free(over[fd]);
 		return (0);
-	}
 	return (1);
 }
