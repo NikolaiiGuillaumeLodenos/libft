eac6cd0ce4c1fef69f00124ffa7382b9f22b913c
Guillaume LODENOS <glodenos@e3r8p19.42.fr>
2016-09-25 21:08:29 +0200

    libft

--- src/ft_putstr_tab.c
+++ src/ft_putstr_tab.c
@@ -6,13 +6,13 @@
 /*   By: glodenos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/25 20:09:13 by glodenos          #+#    #+#             */
-/*   Updated: 2016/09/25 20:48:48 by glodenos         ###   ########.fr       */
+/*   Updated: 2016/09/25 21:07:51 by glodenos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_putstr_tab(char **str)
+void    ft_putstr_tab(char **str, char *sep)
 {
     size_t  i;
 
@@ -20,6 +20,7 @@ void    ft_putstr_tab(char **str)
     while (str[i])
     {
         ft_putstr(str[i]);
+        ft_putstr(sep);
         ++i;
     }
 }
